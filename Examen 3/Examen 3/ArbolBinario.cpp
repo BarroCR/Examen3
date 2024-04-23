1b47b60d73938287503c48006cfb609a3fb6a3dd
BarroCR <rojasbrian84@hotmail.com>
2024-04-23 13:11:47 -0600

    Revert "lito"
    This reverts commit 4acf992227f4071775c5cc2e6f5d6eda80d9746f.

--- Examen 3/Examen 3/ArbolBinario.cpp
+++ Examen 3/Examen 3/ArbolBinario.cpp
@@ -1,5 +1,4 @@
 #include "ArbolBinario.h"
-#include <fstream>
 
 ArbolBinario::ArbolBinario() : raiz(nullptr) {
 
@@ -57,25 +56,11 @@ ArbolBinario::Nodo* ArbolBinario::buscarElemento(Question question) {
 
 void ArbolBinario::hacerPregunta() {
     Nodo* nodoActual = raiz;
-    ofstream archivo("respuestas_chatbot.txt", ios::app);
-
-    if (!archivo.is_open()) {
-        cerr << "No se pudo abrir el archivo para guardar las respuestas." << endl;
-        return;
-    }
-
-    archivo << "Bienvenido al chatbot del examen 3 de la Universidad Latina\n";
     cout << "Bienvenido al chatbot del examen 3 de la Universidad Latina" << endl;
-
     while (nodoActual != nullptr and nodoActual->question.getIsQuestion() == true) {
-
         cout << nodoActual->question.getQuestion() << " (si/no): ";
-        archivo << nodoActual->question.getQuestion() << " (si/no): ";
-
         string respuesta;
         cin >> respuesta;
-
-        archivo << respuesta << "\n"; // Guarda la respuesta en el archivo
 
         if (respuesta == "si") {
             nodoActual = nodoActual->right;
@@ -84,26 +69,20 @@ void ArbolBinario::hacerPregunta() {
             nodoActual = nodoActual->left;
         }
         else {
-            cout << "Respuesta inválida. Por favor, responde con 'si' o 'no'." << endl;
+            cout << "Respuesta inválida. Por favor, responde con 'si' o 'no'." << std::endl;
         }
 
-
+        
     }if (nodoActual != nullptr) {
         cout << nodoActual->question.getQuestion() << endl;
-        archivo << nodoActual->question.getQuestion() << "\n"; // Guarda la pregunta final en el archivo
 
     }
     else {
         cout << "No hay respuestas para tu respuesta " << endl;
-        archivo << "No hay respuestas para tu respuesta\n";
     }
     cout << "Gracias por usar el chat bot!" << endl;
-    archivo << "Gracias por usar el chat bot!\n";
 
-    cout << "¡Fin del chat!" << endl;
-    archivo << "¡Fin del chat!\n\n\n";
-
-    archivo.close();
+    cout << "¡Fin del chat!" << std::endl;
 }
 
 
